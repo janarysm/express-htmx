4b0336abc4377882cbef28121e6e4f2b5a400f94
qq <matayevjanarys@gmail.com>
2024-03-07 20:20:06 +0500

    minimal app setup

--- index.mjs
+++ index.mjs
@@ -1,1 +1,19 @@
-console.log("ok")
+import express from "express";
+import { engine } from "express-handlebars";
+
+const app = express()
+app.enable('view cache')
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get("/", (req, res) => {
+    res.render('home')
+})
+
+try {
+    app.listen(3000)
+    console.log("Server is runing on 3000 port")
+} catch (error) {
+    console.log("Error ------> ", error)
+}
